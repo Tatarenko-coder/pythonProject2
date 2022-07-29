e9f565bd82e0812d8c823c505c423a1c003ef127
Dmytro <104317754+Tatarenko-coder@users.noreply.github.com>
2022-07-30 02:02:47 +0300

    ver 4

--- web/settings.py
+++ web/settings.py
@@ -20,7 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-27wph92!r$dw)sqh&83%wtjcmg(&zngg-04oa&^xt=asno^&)('
+#SECRET_KEY = 'django-insecure-27wph92!r$dw)sqh&83%wtjcmg(&zngg-04oa&^xt=asno^&)('
+import os
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-27wph92!r$dw)sqh&83%wtjcmg(&zngg-04oa&^xt=asno^&)(')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -121,6 +124,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
